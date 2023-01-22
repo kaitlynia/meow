074e6525c2fa776b5ecd775a71c7d80be71c6ac6
Shiroroku(Elise) <elseytd@gmail.com>
2023-01-22 01:06:57 -0800

    text component and camera

--- main.lua
+++ main.lua
@@ -1,20 +1,36 @@
 require "rendering.Rendering"
+require "config"
+require "rendering.Camera"
+require "rendering.components.Button"
 
-Drawables = {}
+GuiDrawables = {}
+WorldDrawables = {}
+
+clientConfig = loadClientConfig("config.json")
+debug_isConnected = false;
 
 function love.load()
 	setupRendering()
-
-	table.insert(Drawables, Button:new("Connect", 640 / 2 - 150, 360 / 2 - 40, 300, 80, function()
+	table.insert(GuiDrawables, Button:new("Connect", 10, 10, 300, 80, function()
 
 	end))
+
 end
+
+furnace = love.graphics.newImage("resource/textures/furnace.png")
 
 function love.draw()
 	love.graphics.push()
 	love.graphics.scale(PixelScaleX, PixelScaleY)
+	camera:set()
 
-	for _, v in pairs(Drawables) do
+	--Draw world stuff here
+	--love.graphics.draw(furnace,20,20)
+
+
+	camera:unset()
+
+	for _, v in pairs(GuiDrawables) do
 		v:draw()
 	end
 
@@ -22,10 +38,9 @@ function love.draw()
 end
 
 function love.update(dt)
-	for _, v in pairs(Drawables) do
+	for _, v in pairs(GuiDrawables) do
 		v:update()
 	end
-
 end
 
 function love.quit()
